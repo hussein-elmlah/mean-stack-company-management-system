dc9474ee6f3756d81d42dcf6156215ebdffe687e
mostafagh10 <ghazalymostafa07@gmail.com>
2024-02-14 15:12:22 +0200

    angular structuring

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+        title:"Home page"
+    },
+    {
+        path:'login',
+        component:LoginComponent,
+        title:"login page"
+    },
+    {
+        path:'signup',
+        component:SignupComponent,
+        title:"signup page"
+    }
+];
