483556c18d0cef779c331c66502d6641b6006f3f
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-02-15 19:54:02 +0200

    SinUp Minimum requirements

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent , RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  signUpForm!: FormGroup;
+  constructor(private router: Router) {
+    this.signUpForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
 
+  handleFormSubmit() {
+    console.log(this.signUpForm);
+    console.log(this.signUpForm.value);
+    this.router.navigate(['/']);
+  }
 }
