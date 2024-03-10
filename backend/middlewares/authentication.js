cfb97892777ebec2910294c7cd22fbb9c578c1c5
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-03-10 23:17:03 +0200

    Fix: small issue in authentication middleware

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -1,15 +1,17 @@
 import jwt from 'jsonwebtoken';
 import asyncHandler from '../lib/asyncHandler.js';
+import User from '../src/modules/user/user.model.js';
 
 export const isAuth = asyncHandler(async (req, res, next) => {
   try {
-    const token = req.header('token');
+    const token = req.header('authorization');
     if (!token) {
       return res.status(403).json('Your are not authenticated please login');
     }
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    if (verified) {
-      req.user = verified.user;
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decodedToken.id).exec();
+    if (decodedToken) {
+      req.user = user;
       next();
     }
   } catch (error) {
