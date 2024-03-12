634234a72ae06e492442550b391b24d59596d72b
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-03-12 15:00:03 +0200

    Feat: enhance get projects route to accept any query params

--- backend/src/modules/project/project.controllers.js
+++ backend/src/modules/project/project.controllers.js
@@ -7,27 +7,16 @@ export const getAllProjects = asyncHandler(async (req, res) => {
   const page = parseInt(req.query.page, 10) || 1; // Default to page 1
   const limit = parseInt(req.query.limit, 10) || 10; // Default limit to 10 projects per page
 
-  // Filter parameters
+  // Construct filters dynamically based on query parameters
   const filters = {};
 
-// Apply filters based on query parameters
-for (const key in req.query) {
-  if (req.query.hasOwnProperty(key)) {
-    if (key === 'page' || key === 'limit') {
-      continue; // Skip pagination parameters
+  // Extract query parameters and construct filters
+  Object.keys(req.query).forEach((param) => {
+    // Exclude pagination parameters
+    if (param !== 'page' && param !== 'limit') {
+      filters[param] = req.query[param];
     }
-    // Handle filters for sub-documents or arrays
-    if (key.includes('.')) {
-      const [parent, child] = key.split('.');
-      const nestedField = `${parent}.${child}`;
-      filters[nestedField] = req.query[key];
-    } else {
-      // Handle direct fields
-      filters[key] = req.query[key];
-    }
-  }
-}
-  console.log(filters);
+  });
 
   // Calculate the index of the first project to retrieve
   const startIndex = (page - 1) * limit;
@@ -51,7 +40,6 @@ for (const key in req.query) {
     totalProjects,
   });
 });
-
 
 export const getProjectById = asyncHandler(async (req, res) => {
   const { projectId } = req.params;
